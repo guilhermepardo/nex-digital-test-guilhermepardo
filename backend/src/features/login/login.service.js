b33a235d29202529fd08a7344ba00e39095c394f
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-06-08 18:36:15 -0300

    adding token generator

--- backend/src/features/login/login.service.js
+++ backend/src/features/login/login.service.js
@@ -1,20 +1,25 @@
 const database = require('../../../config/sequelize/sequelize.config')
 const User = database.users
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 class Service {
 
     async login(body) {
         try {
-            const exists = await User.findOne({
+            const user = await User.findOne({
                 where: {
                     email: body.email
                 }
             })
-            if (!exists) throw { statusCode: 400, message: "User doesn't exist" }
-            const validLogin = await bcrypt.compare(body.password, exists.password)
+            if (!user) throw { statusCode: 400, message: "User doesn't exist" }
+            const validLogin = await bcrypt.compare(body.password, user.password)
             if (!validLogin) throw { statusCode: 401, message: "Invalid credentials" }
-            return exists;
+            const token = jwt.sign(
+                { id: user.id, email: user.email },
+                 process.env.TOKEN_SECRET,
+                { expiresIn: process.env.TOKEN_EXPIRATION  })
+            return { token };
         } catch (error) {
             throw error;
         };
